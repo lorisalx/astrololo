47c495d27e7f0a7d7cfa2ac0ddbfa1728056d12c
Loris ALEXANDRE <loris.alexandre@orange.fr>
2023-02-12 21:02:16 +0100

    fix(types): collections

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,7 +1,17 @@
 import { defineCollection, z } from 'astro:content';
 
 export const collections = {
-	work: defineCollection({
+	workfr: defineCollection({
+		schema: z.object({
+			title: z.string(),
+			description: z.string(),
+			publishDate: z.coerce.date(),
+			tags: z.array(z.string()),
+			img: z.string(),
+			img_alt: z.string().optional(),
+		}),
+	}),
+	worken: defineCollection({
 		schema: z.object({
 			title: z.string(),
 			description: z.string(),
